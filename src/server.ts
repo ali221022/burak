3295c3127812b94a283be2e7d5ff4836431cf176
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-09-02 20:29:50 +0900

    BRR: feat: Integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,3 @@
-// Architectural pattern: MVC, Dependency Injection, MVP
-
-// MVC - MODEL VIEW CONTROLLER
-
-
-// Design pattern: Middleware, Decotar
+import dotenv from 'dotenv';
+dotenv.config();
 
