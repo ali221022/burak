e551ce109f842637a3192991f0e0be47585c101d
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-09-14 07:24:08 +0900

    BRR: feat: frontend related css, js and image creation

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -10,7 +10,7 @@ const restaurantController: T = {};
 restaurantController.goHome = (req: Request, res: Response) => {
   try {
     console.log("goHome");
-    res.send("Home Page");
+    res.render("home");
     // send | json | redirect | end | render
   } catch (err) {
     console.log("ERROR, goHome:", err);
@@ -20,7 +20,7 @@ restaurantController.goHome = (req: Request, res: Response) => {
 restaurantController.getSignup = (req: Request, res: Response) => {
   try {
     console.log("getSignup");
-    res.send("Signup Page");
+    res.render("signup");
   } catch (err) {
     console.log("ERROR, getSignup:", err);
   }
@@ -45,7 +45,7 @@ restaurantController.processSignup = async (req: Request, res: Response) => {
 restaurantController.getLogin = (req: Request, res: Response) => {
   try {
     console.log("getLogin");
-    res.send("Login Page");
+    res.render("login");
   } catch (err) {
     console.log("ERROR, getLogin:", err);
   }
