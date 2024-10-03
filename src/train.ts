d1279bb53414d9bdaa55ef33cab0e0fcd3b09c1c
ali221022 <shohjahon.juraev.4496@mail.ru>
2024-10-03 19:04:05 +0900

    BRR: feat: TACK

--- src/train.ts
+++ src/train.ts
@@ -288,9 +288,36 @@ Qabul qilingan objectni nested array sifatida convert qilib qaytarsin
 MASALAN: objectToArray( {a: 10, b: 20}) return [['a', 10], ['b', 20]]
  */
 
+/** 
 function objectToArray(obj: object)  {
     return Object.entries(obj);
 };
 
 const obj = {"a": 10, "b": 20};
 console.log(objectToArray(obj));
+*/
+
+/**
+ * TASK Q:
+
+Shunday function yozing, u 2 ta parametrga ega bo'lib
+birinchisi object, ikkinchisi string bo'lsin.
+Agar qabul qilinayotgan ikkinchi string, objectning
+biror bir propertysiga mos kelsa, 'true', aks holda mos kelmasa 'false' qaytarsin.
+
+MASALAN: hasProperty({ name: "BMW", model: "M3" }, "model"); return true;
+Ushbu misolda, 'model' string, objectning propertysiga mos kelganligi uchun 'true' natijani qaytarmoqda
+
+MASALAN: hasProperty({ name: "BMW", model: "M3" }, "year"); return false;
+Ushbu misolda, ikkinchi argument sifatida berilayotgan 'year' objectning
+propertysida mavjud bo'lmaganligi uchun 'false' natijani qaytarmoqda.
+
+ */
+
+function hasProperty(obj: object, prop: string) {
+        return obj.hasOwnProperty(prop);
+};
+
+console.log(hasProperty({ name: "BMW", model: "M3" }, "model")); // true
+console.log(hasProperty({ name: "BMW", model: "M3" }, "year"));  // false
+
